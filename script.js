6e585763c24c619d514fe9318e84145583c4d699
leMinhPhuong <phuongleminh841@gmail.com>
2024-03-24 21:28:34 +0700

    construct structure

---
+++ script.js
@@ -0,0 +1,19 @@
+container = document.querySelector(".container");
+console.log(container);
+var initUI = {
+  cacheDOM: function () {},
+  createBoxes: function () {},
+  bindingEvents: function () {},
+  bindingRelationship: function () {},
+};
+var initLogic = {
+  reset: function () {},
+  addX: function () {},
+  addO: function () {},
+  checkWinner: function () {},
+};
+var mediator = {
+  on: function () {},
+  off: function () {},
+  omit: function () {},
+};
